7c77dc4ee8b95adcb76ef7f49524e07edd499690
Steven Tran <steventran@Stevens-MacBook-Pro.local>
2023-03-31 22:36:12 -0400

    Changed my blurb to include notion page

--- components/Blurb.tsx
+++ components/Blurb.tsx
@@ -8,9 +8,20 @@ const Blurb = () => {
           Hey there! I&apos;m Steven, an aspiring full-stack developer, and
           current Computer Science student at the University of Georgia. I love
           learning new technologies and creating personal projects to practice.
-          I enjoy working with Java, TypeScript and React. When I&apos;m not
-          online, you can find me spending time with my two cats and trying out
-          new food in Metro-Atlanta.
+          I enjoy working with Java, TypeScript and React. I keep notes on what
+          I&apos;m learning on my{" "}
+          <a
+            href="https://steven-tran.notion.site/CS-Concept-Notes-a8ade4543472479ea0f3ff3a5263ad7f"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Notion Page
+          </a>
+          , check it out!
+          <br />
+          <br />
+          When I&apos;m not online... you can probably find me spending time
+          with my two cats or trying out new food in Metro-Atlanta.
         </p>
       </div>
     </div>
